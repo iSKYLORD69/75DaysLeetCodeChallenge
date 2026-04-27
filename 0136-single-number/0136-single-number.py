40ebf0f6157ab13b752d04d3af8fd41854b8fb3f
Lucky Singh <rudranshsingh112@gmail.com>
2026-04-27 21:42:11 +0530

    Fix formatting and whitespace in singleNumber method

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,7 +2,7 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         result = 0
 
-        for num in nums:
+        for num in nums: 
             result ^= num
 
-        return result
+        return result 
