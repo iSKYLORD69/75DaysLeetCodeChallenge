82f774332c1fe90057fd0353104290f68eb3816c
Lucky Singh <rudranshsingh112@gmail.com>
2026-05-07 21:16:04 +0530

    Simplify fruit counting logic
    Removed logic for handling more than 2 fruit types.

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -11,17 +11,4 @@ class Solution:
 
             count[fruit] = count.get(fruit, 0) + 1
 
-            # More than 2 fruit types
-            while len(count) > 2:
-                left_fruit = fruits[left]
-
-                count[left_fruit] -= 1
-
-                if count[left_fruit] == 0:
-                    del count[left_fruit]
-
-                left += 1
-
-            max_len = max(max_len, right - left + 1)
-
-        return max_len
+           
