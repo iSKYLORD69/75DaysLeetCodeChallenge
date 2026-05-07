49a1e83f4e0972ea5fa32e55de3f1410806ed4d3
Lucky Singh <rudranshsingh112@gmail.com>
2026-05-07 20:14:19 +0530

    Time: 177 ms (76.67%), Space: 25.8 MB (63.56%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,9 +1,10 @@
 
+
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         left = 0
         count = {}
-        max_fruits = 0
+        max_len = 0
 
         for right in range(len(fruits)):
             fruit = fruits[right]
@@ -13,6 +14,7 @@ class Solution:
             # More than 2 fruit types
             while len(count) > 2:
                 left_fruit = fruits[left]
+
                 count[left_fruit] -= 1
 
                 if count[left_fruit] == 0:
@@ -20,6 +22,6 @@ class Solution:
 
                 left += 1
 
-            max_fruits = max(max_fruits, right - left + 1)
+            max_len = max(max_len, right - left + 1)
 
-        return max_fruits
+        return max_len
